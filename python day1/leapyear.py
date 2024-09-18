c4767e3cee218f444b9d31615672e63f4802ad10
singampalliprakash <jnanaprakash2002@gmail.com>
2024-09-18 22:57:39 +0530

    Delete python day1 directory

--- python day1/leapyear.py
+++
@@ -1,7 +0,0 @@
-year=int(input("enter the year:"))
-if(year%400==0)and(year%100==0):
-    print("year is a leap year",year)
-elif(year%4==0)and(year%100!=0):
-    print("year is a leap year",year)
-else:
-    print("year is not a leap year",year)
--- python day1/reverse of number.py
+++
@@ -1,7 +0,0 @@
-num=int(input("enter the number:"))
-rev=0
-while(num>0):
-    digit=num%10
-    rev=rev*10+digit
-    num=num//10
-print(rev)
